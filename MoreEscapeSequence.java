0c7aebbf9e32b60475945f81729fb8271d97e887
glemma32 <genet.woldeyes@gmail.com>
2017-05-25 14:50:39 -0400

    first commit

---
+++ MoreEscapeSequence.java
@@ -0,0 +1,11 @@
+
+public class MoreEscapeSequence {
+	public static void main(String[] args) {
+	System.out.println("Cost\t\tQuantity\tTotal");
+	System.out.println("$1,000.00\t4\t\t$4,000.00");
+	System.out.println("$   50.00\t8\t\t  $400.00");
+	//System.out.println("");
+	System.out.println("_________________________________________");
+	System.out.println("TOTAL:\t\t12\t\t$4,400.00");
+	}
+}
